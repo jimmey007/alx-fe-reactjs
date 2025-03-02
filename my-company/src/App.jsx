3e6ff9a085d9dcffa1d77267fbac8e4a0b6a6061
jimmey007 <jimmeyousery@gmail.com>
2025-03-02 20:18:39 +0000

    asdc
    s

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar'; // Import the Navbar component
+import Navbar from './Components/Navbar.jsx'; // Import the Navbar component
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar.jsx';
 
 function App() {
   return (
