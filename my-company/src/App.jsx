a48c355296cbb999aa2694f33d4e1f67e0b3df24
jimmey007 <jimmeyousery@gmail.com>
2025-03-02 20:13:40 +0000

    Update App.jsx
    as

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; // Import the Navbar component
+import Navbar from './Components/Navbar'; // Import the Navbar component
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
