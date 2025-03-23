08274a9d3f1b02c1a98c988f1416816d7f97f476
jimmey007 <jimmeyousery@gmail.com>
2025-03-23 10:13:19 +0000

    bbbb

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to your files
-  darkMode: false, // Or 'media'/'class' if you want dark mode
-  theme: { extend: {} }, // Customize your theme here
-  variants: { extend: {} }, // Extend variants if needed
-  plugins: [], // Add plugins here
-};
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
