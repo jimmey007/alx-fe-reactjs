0ee7213c708972cf328da1731a393c7b36c989ea
jimmey007 <jimmeyousery@gmail.com>
2025-03-23 10:09:38 +0000

    dfff

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to your files
+  darkMode: false, // Or 'media'/'class' if you want dark mode
+  theme: { extend: {} }, // Customize your theme here
+  variants: { extend: {} }, // Extend variants if needed
+  plugins: [], // Add plugins here
+};
